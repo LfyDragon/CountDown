c8fa8c9fec48931ab0193df2155a99115ddcf397
ios2chen <ioschen@peerkin.com>
2017-08-23 16:06:00 +0800

    eeqwer

---
+++ qwer/qwer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qwer
+//
+//  Created by ios2chen on 2017/8/23.
+//  Copyright © 2017年 Lfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
